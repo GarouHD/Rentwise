08ef5750370731ca25fdbd981a6b2b165587281a
garouhd <ziyadsbeih0.0@gmail.com>
2025-10-29 02:41:47 -0400

    first rentwise commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header')
+
+  @if (! have_posts())
+    <x-alert type="warning">
+      {!! __('Sorry, no results were found.', 'sage') !!}
+    </x-alert>
+
+    {!! get_search_form(false) !!}
+  @endif
+
+  @while(have_posts()) @php(the_post())
+    @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+  @endwhile
+
+  <div class="text-red-400 font-bold text-5xl">Tailwind is working if this is red and bold</div>
+
+  {!! get_the_posts_navigation() !!}
+@endsection
+
+@section('sidebar')
+  @include('sections.sidebar')
+@endsection
